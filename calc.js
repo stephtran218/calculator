0c552bc19f51e4256ed9e33dd48471ee853c0c4d
StudentAM <studentam@Administrators-MacBook-Pro.local>
2024-01-11 10:27:41 -0800

    1/11

--- calc.js
+++ calc.js
@@ -1,4 +1,35 @@
-let numButtons = document.getElementById("button")
-let clear = document.getElementById("clearBtn")
+let numButtons = document.querySelectorAll("button");
+let clear = document.getElementById("clearBtn");
+let display = document.getElementById("numDisplay");
 
-function appearNumbers();
+function appearNumbers(event){
+    const buttonValue = 
+}
+
+function appearNumbers(event){
+    const buttonValue = event.target.dataset.value;
+    updateDisplay(buttonValue);
+}
+
+function addBtn(){
+    const currentInput = display.textContent;
+
+}
+
+function updateDisplay(value) {
+    const currentInput = display.textContent;
+
+    if (currentInput === '0' || isNaN(parseFloat(currentInput))) {
+        display.textContent = value;
+    } else {
+        display.textContent += value;
+    }
+}
+
+for (let i = 0; i < numButtons.length; i++) {
+    numButtons[i].addEventListener("click", appearNumbers);
+}
+
+clear.addEventListener("click", function () {
+    display.textContent = '0';
+});
