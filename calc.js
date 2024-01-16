ee7492e394597f430cdaada6048bc798fc033713
StudentAM <studentam@Administrators-MacBook-Pro.local>
2024-01-16 10:28:22 -0800

    1/16

--- calc.js
+++ calc.js
@@ -1,93 +1,120 @@
-let numButtons = document.querySelectorAll("button");
-let clear = document.getElementById("clearBtn");
-let display = document.getElementById("numDisplay");
-let addBtn = document.getElementById("addBtn");
-let subBtn = document.getElementById("subBtn");
-let multBtn = document.getElementById("multBtn");
+let numButtons = document.querySelectorAll(".button");
 let divBtn = document.getElementById("divBtn");
-let button1 = document.getElementById("button-1");
-let button2 = document.getElementById("button-2");
-let button3 = document.getElementById("button-3");
-let button4 = document.getElementById("button-4");
-let button5 = document.getElementById("button-5");
-let button6 = document.getElementById("button-6");
-let button7 = document.getElementById("button-7");
-let button8 = document.getElementById("button-8");
-let button9 = document.getElementById("button-9");
+let multBtn = document.getElementById("multBtn");
+let subBtn = document.getElementById("subBtn");
+let addBtn = document.getElementById("addBtn");
+let equalBtn = document.getElementById("equalBtn");
+let clear = document.getElementById("clearBtn");
+let decimalBtn = document.getElementById("decimalBtn");
+let display = document.getElementById("numDisplay");
 
-let currentInput = 0;
+let startingInput = 0;
+let currentInput = "";
+let secondInput = "";
+let operator = "";
 
-//This function will make the buttons's values the user clicked to display on screen
 function appearNumbers(event) {
-    const buttonValue = event.target.textContent;
-    updateDisplay(buttonValue);
+    let buttonClicked = event.target;
+    currentInput += buttonClicked.innerText;
+    display.innerText = currentInput;
 }
 
-function updateDisplay(buttonValue) {
-    const currentInput = display.innerText;
-
-    if (currentInput === '0' || isNaN(parseFloat(currentInput))) {
-        display.innerText = buttonValue;
-    } else {
-        display.innerText += buttonValue;
-    }
+function handleNumberClick(number) {
+    currentInput += number;
+    appearNumbers();
 }
 
-function resetValue() {
-    display.innerText = '0';
-    currentOperator = null;
-    firstOperand = null;
-}
-
-function setOperator(operator) {
-    if (firstOperand === null) {
-        firstOperand = parseFloat(display.innerText);
-        currentOperator = operator;
-        display.innerText = '0';
-    } else {
-        calcExpression();
-        currentOperator = operator;
-    }
-}
-
-function calcExpression() {
-    const secondOperand = parseFloat(display.innerText);
-
-    if (!isNaN(firstOperand) && !isNaN(secondOperand) && currentOperator !== null) {
-        let result;
-
-        switch (currentOperator) {
-            case '+':
-                result = firstOperand + secondOperand;
-                break;
-            case '-':
-                result = firstOperand - secondOperand;
-                break;
-            case '*':
-                result = firstOperand * secondOperand;
-                break;
-            case '/':
-                result = firstOperand / secondOperand;
-                break;
-            default:
-                break;
+function addNumbers() {
+    if (currentInput !== "") {
+        secondInput = parseFloat(currentInput);
+        if (!isNaN(secondInput)) {
+            operator = "+";
+            currentInput = "";
+            display.innerText = startingInput;
+        } else {
+            display.innerText = "Error";
         }
-
-        display.innerText = result.toString();
-        currentOperator = null;
-        firstOperand = result;
     }
+}
+
+function subNumbers() {
+    if (currentInput !== "") {
+        secondInput = parseFloat(currentInput);
+        if (!isNaN(secondInput)) {
+            operator = "-";
+            currentInput = "";
+            display.innerText = startingInput;
+        } else {
+            display.innerText = "Error";
+        }
+    }
+}
+
+function multNumbers() {
+    if (currentInput !== "") {
+        secondInput = parseFloat(currentInput);
+        if (!isNaN(secondInput)) {
+            operator = "*";
+            currentInput = "";
+            display.innerText = startingInput;
+        } else {
+            display.innerText = "Error";
+        }
+    }
+}
+
+function divNumbers() {
+    if (currentInput !== "") {
+        secondInput = parseFloat(currentInput);
+        if (!isNaN(secondInput)) {
+            operator = "/";
+            currentInput = "";
+            display.innerText = startingInput;
+        } else {
+            display.innerText = "Error";
+        }
+    }
+}
+
+function calculateResult() {
+    if (currentInput !== "") {
+        let result = parseFloat(currentInput);
+        if (!isNaN(result)) {
+            if (operator === "+") {
+                startingInput += result;
+            } else if (operator === "-") {
+                startingInput -= result;
+            } else if (operator === "*") {
+                startingInput *= result;
+            } else if (operator === "/") {
+                startingInput /= result;
+            }
+
+            currentInput = "";
+            operator = ""; // Reset the operator after calculation
+            display.innerText = startingInput;
+        } else {
+            display.innerText = "Error";
+        }
+    }
+}
+
+function clearDisplay() {
+    startingInput = 0;
+    currentInput = "";
+    secondInput = "";
+    operator = "";
+    display.innerText = "0";
 }
 
 for (let i = 0; i < numButtons.length; i++) {
     numButtons[i].addEventListener("click", appearNumbers);
 }
 
-addBtn.addEventListener("click", () => setOperator('+'));
-subBtn.addEventListener("click", () => setOperator('-'));
-multBtn.addEventListener("click", () => setOperator('*'));
-divBtn.addEventListener("click", () => setOperator('/'));
+addBtn.addEventListener("click", addNumbers);
+subBtn.addEventListener("click", subNumbers);
+multBtn.addEventListener("click", multNumbers);
+divBtn.addEventListener("click", divNumbers);
 
-document.getElementById("equalBtn").addEventListener("click", calcExpression);
-
-clear.addEventListener("click", resetValue);
+equalBtn.addEventListener("click", calculateResult);
+clear.addEventListener("click", clearDisplay);
